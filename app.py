d892d23266ae8f133cb9ff707837d64a97e52f7f
azhuang <azhuang@umich.edu>
2021-11-06 01:41:28 -0400

    added pymongo to requirements

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__) #might have to update static_folder variable
 
@@ -6,3 +7,7 @@ app = Flask(__name__) #might have to update static_folder variable
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/buildings')
+def get_buildings():
+    client = MongoClient()
