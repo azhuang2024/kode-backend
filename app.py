7ccb070e68682f60085870a723b4bf8819db5288
azhuang <azhuang@umich.edu>
2021-11-07 01:35:43 -0400

    MongoDB up and running

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pymongo
 from pymongo import MongoClient
 
 app = Flask(__name__) #might have to update static_folder variable
-
+cluster = MongoClient('mongodb+srv://gurish:1234@cluster0.98ccl.mongodb.net/kode?retryWrites=true&w=majority')
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
-@app.route('/buildings')
+@app.route('/buildings/', methods=['POST', 'GET'])
 def get_buildings():
-    client = MongoClient()
+
+    db = cluster.kode
+    buildingDB = db.buildings
+    results = buildingDB.find({})
+    list = []
+    for result in results:
+        list.append(result)
+    return jsonify(list)
